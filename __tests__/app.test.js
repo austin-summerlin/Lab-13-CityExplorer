7a66b32ff9699b576a925a1306c90d4e408438a0
Austin Summerlin <summerlin.austin@gmail.com>
2021-05-12 17:51:09 -0700

    All Tests Working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,7 @@
 import cityData from '../data/data';
 import weatherData from '../data/weather';
-import { formatCity, formatWeather } from '../lib/munge-utils';
+import yelpData from '../data/yelp';
+import { formatCity, formatWeather, formatYelp } from '../lib/munge-utils';
 
 describe('API Data Munging', () => {
 
@@ -19,6 +20,16 @@ describe('API Data Munging', () => {
     }
   ];
 
+  const expectedYelp = [
+    {
+      'name': 'Mustafas Gemüse Kebap',
+      'image_url': 'https://s3-media2.fl.yelpcdn.com/bphoto/ZNGf-CMpNZ1XktzyFIvGSw/o.jpg',
+      'price': '€',
+      'rating': 4.0,
+      'url': 'https://www.yelp.com/biz/mustafas-gem%C3%BCse-kebap-berlin-3?adjust_creative=CZ_hkgx7WUTh0TKKgoyHJw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=CZ_hkgx7WUTh0TKKgoyHJw'
+    }
+  ];
+
   test('munges city data', async () => {
     const output = formatCity(cityData);
     expect(output).toEqual(expectedStuff);
@@ -27,5 +38,10 @@ describe('API Data Munging', () => {
   test('munges weather data', async () => {
     const output = formatWeather(weatherData);
     expect(output).toEqual(expectedWeather);
+  });
+
+  test('munges yelp data', async () => {
+    const output = formatYelp(yelpData);
+    expect(output).toEqual(expectedYelp);
   });
 });
---
+++ data/yelp.js
@@ -0,0 +1,50 @@
+const yelp =
+{
+  'businesses': [
+    {
+      'id': 'F2HUfsf0sBzkjXWiM93ymg',
+      'alias': 'mustafas-gemüse-kebap-berlin-3',
+      'name': 'Mustafas Gemüse Kebap',
+      'image_url': 'https://s3-media2.fl.yelpcdn.com/bphoto/ZNGf-CMpNZ1XktzyFIvGSw/o.jpg',
+      'is_closed': false,
+      'url': 'https://www.yelp.com/biz/mustafas-gem%C3%BCse-kebap-berlin-3?adjust_creative=CZ_hkgx7WUTh0TKKgoyHJw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=CZ_hkgx7WUTh0TKKgoyHJw',
+      'review_count': 1075,
+      'categories': [
+        {
+          'alias': 'foodstands',
+          'title': 'Food Stands'
+        },
+        {
+          'alias': 'kebab',
+          'title': 'Kebab'
+        }
+      ],
+      'rating': 4.0,
+      'coordinates': {
+        'latitude': 52.493825,
+        'longitude': 13.3880366380635
+      },
+      'transactions': [],
+      'price': '€',
+      'location': {
+        'address1': 'Mehringdamm 32',
+        'address2': '',
+        'address3': '',
+        'city': 'Berlin',
+        'zip_code': '10961',
+        'country': 'DE',
+        'state': 'BE',
+        'display_address': [
+          'Mehringdamm 32',
+          '10961 Berlin',
+          'Germany'
+        ]
+      },
+      'phone': '',
+      'display_phone': '',
+      'distance': 1302.9526779555733
+    }
+  ]
+};
+
+export default yelp;
